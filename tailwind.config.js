c8364bf78987eb18918385305368592b2d3eb6ae
lhamouche <luxel.hamouche.lh@gmail.com>
2024-03-13 22:08:36 +0100

    feat(styles): configure Tailwind CSS and add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{vue,js}",
   ],
   theme: {
+    colors: {
+      'text': '#1D0202',
+      'background': '#FFFFFF',
+      'primary': '#FFE41E',
+      'secondary': '#D7F581',
+      'accent': '#C8A509',
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
