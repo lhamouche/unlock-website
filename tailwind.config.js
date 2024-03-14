fc747df8c98e7cd2abe6693733573f721a1f5b24
lhamouche <luxel.hamouche.lh@gmail.com>
2024-03-14 23:15:13 +0100

    chore(styles): edit Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,18 @@ export default {
     "./src/**/*.{vue,js}",
   ],
   theme: {
-    colors: {
-      'text': '#1D0202',
-      'background': '#FFFFFF',
-      'primary': '#FFE41E',
-      'secondary': '#D7F581',
-      'accent': '#C8A509',
-      ...colors,
-    },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#1D0202',
+        'background': '#FFFFFF',
+        'primary': '#FFE41E',
+        'secondary': '#D7F581',
+        'accent': '#C8A509',
+      },
+    },
   },
   plugins: [],
 }
