dc5ae182a3b9d7f0821e30c3dc9d0fc5c0a69487
lhamouche <luxel.hamouche.lh@gmail.com>
2024-03-18 22:44:04 +0100

    feat(landing-section): add logo banner

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -4,8 +4,8 @@ import LogoBanner from './LogoBanner.vue';
 </script>
 
 <template>
-  <section class="flex h-screen flex-col justify-end bg-background pt-20 font-poppins lg:justify-center">
-    <div class="min-h-2/5 mx-auto flex w-11/12 max-w-screen-xl flex-col justify-between gap-y-6 lg:gap-y-10">
+  <section class="flex h-screen flex-col justify-end bg-background pt-20 font-poppins">
+    <div class="min-h-2/5 mx-auto flex h-full w-11/12 max-w-screen-xl flex-col justify-end gap-y-6 pb-5 lg:justify-center lg:gap-y-10">
       <h1 class="max-w-3xl text-4xl font-bold leading-relaxed lg:text-5xl lg:leading-normal">
         L'association
         <span class="whitespace-nowrap">
@@ -16,8 +16,8 @@ import LogoBanner from './LogoBanner.vue';
         </span>
         de l'ENSEIRB-MATMECA
       </h1>
-      <p class="w-9/12 text-xl">Le numérique au service des étudiants et de l'associatif</p>
-      <div class="flex flex-row flex-wrap gap-4">
+      <p class="w-10/12 pb-2 text-xl">Le numérique au service des étudiants et de l'associatif</p>
+      <div class="flex flex-row flex-wrap gap-5">
         <LandingButton text="Nous découvrir" color="bg-primary" />
         <LandingButton text="Assistance" color="bg-secondary" />
       </div>
---
+++ src/components/LogoBanner.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { gsap } from 'gsap';
+
+const logos = [
+  { name: 'Eirbware', url: 'round_logo_eirbware.svg' }
+];
+
+const lw1 = ref(null);
+const lw2 = ref(null);
+
+const getImageUrl = (name) => new URL(`../assets/logos/${name}`, import.meta.url).href;
+const imageUrl = (url) => getImageUrl(url);
+
+onMounted(() => {
+  const tl = gsap.timeline({ repeat: -1 });
+  tl.fromTo([lw1.value, lw2.value], { x: 0 }, { x: '-100%', duration: 30, ease: 'linear' });
+});
+</script>
+
+<template>
+  <div class="flex h-36 w-full flex-row overflow-hidden lg:h-48">
+    <div v-for="w in ['lw1', 'lw2']" :ref="w" class="flex min-w-full shrink-0 flex-row items-center justify-around">
+      <img v-for="l in logos" :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28" />
+    </div>
+  </div>
+</template>
