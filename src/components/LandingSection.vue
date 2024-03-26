63d95efc3b59c14b431a448cad4a0469159c11f1
lhamouche <luxel.hamouche.lh@gmail.com>
2024-03-26 20:11:19 +0100

    fix(landing-section): adjust position of first image

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -5,11 +5,16 @@ import LogoBanner from './LogoBanner.vue';
 
 <template>
   <section class="flex h-screen flex-col justify-end bg-background pt-20 font-poppins">
-    <div class="min-h-2/5 mx-auto flex h-full w-11/12 max-w-screen-xl flex-col justify-end gap-y-[4%] pb-5 lg:justify-center lg:gap-y-[7%]">
-      <div class="ml-[45%] -mb-3 sm:ml-[55%]">
-        <img src="../assets/img/Core_3.svg" class="-rotate-18 h-36 sm:h-44 lg:h-56 xl:h-64 lg:absolute lg:bottom-[22%] lg:right-[30%] xl:right-[35%]"> <!-- lg:right-[15%] xl:right-[30%] -->
+    <div
+      class="min-h-2/5 mx-auto flex h-full w-11/12 max-w-screen-xl flex-col justify-end gap-y-[4%] pb-5 lg:justify-center lg:gap-y-[7%]"
+    >
+      <div class="-mb-3 ml-[45%] sm:ml-[55%]">
+        <img
+          src="../assets/img/Core_3.svg"
+          class="-rotate-18 h-36 sm:h-44 lg:absolute lg:bottom-[22%] lg:right-[25%] lg:h-56 xl:right-[30%] 2xl:right-[35%] xl:h-64"
+        />
       </div>
-      <h1 class="max-w-3xl text-4xl font-bold leading-relaxed lg:text-5xl lg:leading-normal lg:-mt-20">
+      <h1 class="max-w-3xl text-4xl font-bold leading-relaxed lg:-mt-20 lg:text-5xl lg:leading-normal">
         L'association
         <span class="whitespace-nowrap">
           d'
