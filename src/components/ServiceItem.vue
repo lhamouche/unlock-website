f94838cea112889032f6898cdba556e81e4d4899
lhamouche <luxel.hamouche.lh@gmail.com>
2024-03-31 00:33:36 +0100

    refactor(services-section): move services icons from img to icons

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -5,7 +5,7 @@ const props = defineProps({
   text: String,
 });
 
-const getImageUrl = (name) => new URL(`../assets/img/${name}`, import.meta.url).href;
+const getImageUrl = (name) => new URL(`../assets/icons/${name}`, import.meta.url).href;
 const imageUrl = (url) => getImageUrl(url);
 </script>
 
@@ -13,7 +13,7 @@ const imageUrl = (url) => getImageUrl(url);
   <div
     class="m-auto flex h-[28rem] max-w-[25rem] flex-col items-center gap-9 rounded-3xl bg-secondary bg-opacity-20 px-10 py-16 font-poppins"
   >
-    <img :src="imageUrl(props.icon)" alt="Icon Assistance" class="h-20" />
+    <img :src="imageUrl(props.icon)" alt="Icon" class="h-20" />
     <h3 class="text-4xl font-medium">{{ props.title }}</h3>
     <p>{{ props.text }}</p>
   </div>
