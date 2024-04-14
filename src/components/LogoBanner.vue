f743d249fe1407df3d337b997dbba7ce0ecb1523
lhamouche <luxel.hamouche.lh@gmail.com>
2024-04-14 11:18:09 +0200

    fix(landing-section): fix logo banner animation not working on production

--- src/components/LogoBanner.vue
+++ src/components/LogoBanner.vue
@@ -3,11 +3,25 @@ import { ref, onMounted } from 'vue';
 import { gsap } from 'gsap';
 
 const logos = [
-  { name: 'Eirbware', url: 'round_logo_eirbware.svg' }
+  // { name: 'BAE', url: 'bae.png' },
+  // { name: 'BDA', url: 'bda.png' },
+  // { name: 'BDE', url: 'bde.png' },
+  // { name: 'EirSport', url: 'eirsport.png' },
+  // { name: 'Gala Mos\'fête', url: 'gala.png' },
+  // { name: 'Le Bar', url: 'bar.png' },
+  // { name: 'Bill\'eirb', url: 'billeirb.png' },
+  // { name: 'Cook\'eirb', url: 'cookeirb.png' },
+  // { name: 'CherieF\'eirb', url: 'cheriefeirb.png' },
+  // { name: 'EMK', url: 'emk.png' },
+  // { name: 'Game Creation Club', url: 'gcc.png' },
+  // { name: 'Œno', url: 'oeno.png' },
+  // { name: 'Pix\'eirb', url: 'pixeirb.png' },
+  // { name: 'Club Théâtre', url: 'theatre.png' },
+  // { name: 'Unlock', url: 'unlock.png' },
+  // { name: 'Zik', url: 'zik.png' },
 ];
 
-const lw1 = ref(null);
-const lw2 = ref(null);
+const [lw1, lw2] = [ref(null), ref(null)];
 
 const getImageUrl = (name) => new URL(`../assets/logos/${name}`, import.meta.url).href;
 const imageUrl = (url) => getImageUrl(url);
@@ -20,8 +34,11 @@ onMounted(() => {
 
 <template>
   <div class="flex h-36 w-full flex-row overflow-hidden lg:h-48">
-    <div v-for="w in ['lw1', 'lw2']" :ref="w" class="flex min-w-full shrink-0 flex-row items-center justify-around">
-      <img v-for="l in logos" :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28" />
+    <div ref="lw1" class="flex min-w-full shrink-0 flex-row items-center justify-around">
+      <img v-for="l in logos" :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28 rounded-full" />
+    </div>
+    <div ref="lw2" class="flex min-w-full shrink-0 flex-row items-center justify-around">
+      <img v-for="l in logos" :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28 rounded-full" />
     </div>
   </div>
 </template>
