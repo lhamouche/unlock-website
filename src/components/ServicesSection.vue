cdab0d5fa905f9bc702c9ee09be108c933ff9ec9
lhamouche <luxel.hamouche.lh@gmail.com>
2024-04-25 15:00:11 +0200

    feat(services-section): add section tag

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -22,26 +22,28 @@ const services = [
 </script>
 
 <template>
-  <GradientTitle text="Nos" coloredText="services" />
-  <div class="m-auto flex w-11/12 md:max-w-[44rem] xl:md:max-w-[67rem] 2xl:max-w-[80rem] md:justify-between flex-wrap justify-center gap-8 2xl:gap-14 font-poppins">
-    <ServiceItem v-for="s in services" :icon="s.icon" :title="s.title" :text="s.text" />
-    <img src="../assets/img/bee.svg" alt="Abeille" class="m-auto hidden h-44 -rotate-12 xl:block pl-5" />
-    <div
-      class="flex h-[28rem] max-w-[24rem] md:w-[21rem] xl:max-w-max xl:w-[50rem] 2xl:w-[60rem] xl:h-auto xl:items-start flex-col items-center gap-9 rounded-3xl bg-accent bg-opacity-20 px-10 py-14 overflow-hidden"
-    >
-      <h3 class="text-3xl xl:text-4xl font-medium">
-        Le portail
-        <span class="bg-gradient-to-r from-primary to-accent bg-clip-text font-semibold italic text-transparent">
-          eirb.fr
-        </span>
-      </h3>
-      <p>
-        A travers la page
-        <span class="italic">eirb.fr</span>
-        , nous proposons un accès rapide à l'ensemble des ressources utilisés par les élèves au quotidient : ENT,
-        syllabus, emploi du temps, Moodle, sites des clubs et assos ...
-      </p>
-      <img src="../assets/img/bee.svg" alt="Abeille" class="h-48 rotate-6 xl:hidden" />
+  <section class="m-auto w-11/12 md:max-w-[44rem] xl:md:max-w-[67rem] 2xl:max-w-[80rem]">
+    <GradientTitle text="Nos" coloredText="services" />
+    <div class="flex flex-wrap justify-center gap-8 font-poppins md:justify-between 2xl:gap-14">
+      <ServiceItem v-for="s in services" :icon="s.icon" :title="s.title" :text="s.text" />
+      <img src="../assets/img/bee.svg" alt="Abeille" class="m-auto hidden h-44 -rotate-12 pl-5 xl:block" />
+      <div
+        class="flex h-[28rem] max-w-[24rem] flex-col items-center gap-9 overflow-hidden rounded-3xl bg-accent bg-opacity-20 px-10 py-14 md:w-[21rem] xl:h-auto xl:w-[50rem] xl:max-w-max xl:items-start 2xl:w-[60rem]"
+      >
+        <h3 class="text-3xl font-medium xl:text-4xl">
+          Le portail
+          <span class="bg-gradient-to-r from-primary to-accent bg-clip-text font-semibold italic text-transparent">
+            eirb.fr
+          </span>
+        </h3>
+        <p>
+          A travers la page
+          <span class="italic">eirb.fr</span>
+          , nous proposons un accès rapide à l'ensemble des ressources utilisés par les élèves au quotidient : ENT,
+          syllabus, emploi du temps, Moodle, sites des clubs et assos ...
+        </p>
+        <img src="../assets/img/bee.svg" alt="Abeille" class="h-48 rotate-6 xl:hidden" />
+      </div>
     </div>
-  </div>
+  </section>
 </template>
