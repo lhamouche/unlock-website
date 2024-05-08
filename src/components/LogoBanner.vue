d630c4cee0f87f8faf8ca2583031f3170379d9ff
lhamouche <luxel.hamouche.lh@gmail.com>
2024-05-08 15:33:40 +0200

    feat(landing-section): add logos in logo banner

--- src/components/LogoBanner.vue
+++ src/components/LogoBanner.vue
@@ -3,22 +3,22 @@ import { ref, onMounted } from 'vue';
 import { gsap } from 'gsap';
 
 const logos = [
-  // { name: 'BAE', url: 'bae.png' },
-  // { name: 'BDA', url: 'bda.png' },
-  // { name: 'BDE', url: 'bde.png' },
-  // { name: 'EirSport', url: 'eirsport.png' },
-  // { name: 'Gala Mos\'fête', url: 'gala.png' },
-  // { name: 'Le Bar', url: 'bar.png' },
-  // { name: 'Bill\'eirb', url: 'billeirb.png' },
-  // { name: 'Cook\'eirb', url: 'cookeirb.png' },
-  // { name: 'CherieF\'eirb', url: 'cheriefeirb.png' },
-  // { name: 'EMK', url: 'emk.png' },
-  // { name: 'Game Creation Club', url: 'gcc.png' },
-  // { name: 'Œno', url: 'oeno.png' },
-  // { name: 'Pix\'eirb', url: 'pixeirb.png' },
-  // { name: 'Club Théâtre', url: 'theatre.png' },
-  // { name: 'Unlock', url: 'unlock.png' },
-  // { name: 'Zik', url: 'zik.png' },
+  { name: 'BAE', url: 'bae.png', link: 'https://bae.eirb.fr' },
+  { name: 'BDA', url: 'bda.png', link: 'https://bda.eirb.fr' },
+  { name: 'BDE', url: 'bde.png', link: 'https://bde.eirb.fr' },
+  { name: 'EirSport', url: 'eirsport.png', link: 'https://eirsport.eirb.fr' },
+  { name: 'Gala Mos\'fête', url: 'gala.png', link: 'https://gala.eirb.fr' },
+  { name: 'Le Bar', url: 'bar.png', link: 'https://bar.eirb.fr' },
+  { name: 'Bill\'eirb', url: 'billeirb.png', link: 'https://bill.eirb.fr' },
+  { name: 'Cook\'eirb', url: 'cookeirb.png', link: 'https://cook/eirb.fr' },
+  { name: 'CherieF\'eirb', url: 'cheriefeirb.png', link: 'https://radio.eirb.fr' },
+  { name: 'EMK', url: 'emk.png', link: 'https://emk.eirb.fr' },
+  { name: 'Game Creation Club', url: 'gcc.png', link: 'https://gcc.eirb.fr' },
+  { name: 'Œno', url: 'oeno.png', link: 'https://oeno.eirb.fr' },
+  { name: 'Pix\'eirb', url: 'pixeirb.png', link: 'https://pix.eirb.fr' },
+  { name: 'Club Théâtre', url: 'theatre.png', link: 'https://theatre.eirb.fr' },
+  { name: 'Unlock', url: 'unlock.png', link: 'https://unlock.eirb.fr' },
+  { name: 'Zik', url: 'zik.png', link: 'https://zik.eirb.fr' },
 ];
 
 const [lw1, lw2] = [ref(null), ref(null)];
@@ -35,10 +35,10 @@ onMounted(() => {
 <template>
   <div class="flex h-36 w-full flex-row overflow-hidden lg:h-48">
     <div ref="lw1" class="flex min-w-full shrink-0 flex-row items-center justify-around">
-      <img v-for="l in logos" :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28 rounded-full" />
+      <a v-for="l in logos" :href="l.link" target="_blank"><img :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28 rounded-full" /></a>
     </div>
     <div ref="lw2" class="flex min-w-full shrink-0 flex-row items-center justify-around">
-      <img v-for="l in logos" :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28 rounded-full" />
+      <a v-for="l in logos" :href="l.link" target="_blank"><img :src="imageUrl(l.url)" :alt="'Logo ' + l.name" class="mx-4 h-20 lg:h-28 rounded-full" /></a>
     </div>
   </div>
 </template>
